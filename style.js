cb0480e6b94619df90da6edf8d0696ec417d96be
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2023-05-03 02:41:37 +0530

    chore: Update styling, added js styling, removed html separators

---
+++ style.js
@@ -0,0 +1,13 @@
+const divOne = document.getElementById("div1");
+const divTwo = document.getElementById("div2");
+
+divOne.style.color = "white";
+divOne.style.background = "maroon";
+divOne.style.padding = "2%";
+divOne.style.textAlign = "right";
+
+divTwo.style.color = "white";
+divTwo.style.background = "purple";
+divTwo.style.padding = "2%";
+divTwo.style.marginTop = "1%";
+divTwo.style.textAlign = "left";
